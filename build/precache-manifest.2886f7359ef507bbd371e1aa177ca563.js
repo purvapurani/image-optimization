dcc70913bb104a9c9db6f90475862a3ec7f1c8a5
Purva Purani <purva@orientsoftware.in>
2019-01-29 15:50:49 +0530

    Created build

---
+++ build/precache-manifest.2886f7359ef507bbd371e1aa177ca563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff9ee020fc2cebc6c2a",
+    "url": "/static/css/main.15a6d5cf.chunk.css"
+  },
+  {
+    "revision": "1ff9ee020fc2cebc6c2a",
+    "url": "/static/js/main.1ff9ee02.chunk.js"
+  },
+  {
+    "revision": "d6c2c8a623b817e29929",
+    "url": "/static/js/1.d6c2c8a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad790f65ce07a7526f9640c845cda284",
+    "url": "/index.html"
+  }
+];
